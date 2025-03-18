c6ce02e824f534ccf76d7ca0aff86c7cfd99ad30
Navneet Thakur <thakurnavneet686@gmail.com>
2025-03-18 23:35:43 +0530

    settingAngular2

---
+++ landingPage/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { NgOptimizedImage } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ShopComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgOptimizedImage
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ landingPage/src/app/shop/shop.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop',
+  standalone: false,
+  templateUrl: './shop.component.html',
+  styleUrl: './shop.component.scss'
+})
+export class ShopComponent {
+
+}
